9f03a4d869596229275778947ff2cea836dd1db3
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-11 11:55:13 -0600

    rearranged setup to move more into the test

--- src/test/catalyst-sdlc-integration.test.mjs
+++ src/test/catalyst-sdlc-integration.test.mjs
@@ -2,8 +2,20 @@
 
 import { tryExec } from '@liquid-labs/shell-toolkit'
 
-describe('initial setup', () => {
-  test('foo', () => {
-    tryExec('catalyst server plugins bundles add -- installBundles=catalyst-sdlc-node')
+const CLI_COMMAND='sdlc'
+const CLI_PACKAGE='sdlcpilot-cli'
+const SERVER_PACKAGE='comply-server'
+
+describe('sdlcpilot-github-node', () => {
+  beforeAll(() => {
+    tryExec(`npm i -g ${SERVER_PACKAGE}`)
+    tryExec(`npm i -g ${CLI_PACKAGE}`)
+    tryExec(`TERMINAL_STYLE=greenOnBlack TERMINAL_WIDTH=0 ${CLI_COMMAND} --setup`)
+    tryExec(`${CLI_COMMAND} server plugins bundles add -- installBundles=catalyst-sdlc-node`)
+  })
+
+  test('loads 10 handler plugins (plus original core)', () => {
+    const result = tryExec(`${CLI_COMMAND} server plugins handlers list | wc -l`)
+    expect(result.stdout.trim()).toBe('11')
   })
 })
