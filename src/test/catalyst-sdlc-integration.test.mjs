700890a0218f0cce18421cbea2536e6d24e80e02
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-12 15:52:50 -0600

    reworked setup to be non-blocking and provide a running narrative for debug

--- src/test/catalyst-sdlc-integration.test.mjs
+++ src/test/catalyst-sdlc-integration.test.mjs
@@ -4,13 +4,25 @@ import { tryExec } from '@liquid-labs/shell-toolkit'
 
 const CLI_COMMAND='sdlc'
 const CLI_PACKAGE='sdlcpilot-cli'
+const SERVER_COMMAND=`comply-server`
 const SERVER_PACKAGE='comply-server'
 
 describe('sdlcpilot-github-node', () => {
-  beforeAll(() => {
+  beforeAll(async () => {
+    process.stdout.write(`Installing ${SERVER_PACKAGE} and ${CLI_PACKAGE}...`)
     tryExec(`npm i -g ${SERVER_PACKAGE}`)
     tryExec(`npm i -g ${CLI_PACKAGE}`)
+    process.stdout.write('Running setup...')
     tryExec(`TERMINAL_STYLE=greenOnBlack TERMINAL_WIDTH=-1 ${CLI_COMMAND} --setup`)
+    process.stdout.write('Starting server...')
+    new Promise((resolve) => {
+      tryExec(SERVER_COMMAND)
+      resolve()
+    })
+      .then(() => { process.stdout.write('Server process has exitted.') })
+    process.stdout.write('Giving it time to spin up...')
+    await new Promise((resolve) => setTimeout(resolve, 20 * 1000 /* 20 seconds */))
+    process.stdout.write('Installing bundle...')
     tryExec(`${CLI_COMMAND} server plugins bundles add -- bundles=catalyst-sdlc-node`)
   })
 
